3b7b6d750898eb8e0bcf5a06fe33233198a9310c
guilhermeinovando <guilherme@inovando.online>
2021-06-15 14:24:00 -0300

    sync

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,46 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'flexible.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: DefaultTabController(
-        length: 2,
-        child: CustomScrollView(
-          slivers: <Widget>[
-            SliverPersistentHeader(
-              pinned: true,
-              floating: false,
-              delegate: NetworkingPageHeader(
-                minExtent: kToolbarHeight + MediaQuery.of(context).padding.top,
-                maxExtent: 250.0,
-              ),
-            ),
-            SliverPersistentHeader(
-              delegate: SliverAppBarDelegate(
-                TabBar(
-                  labelColor: Colors.black87,
-                  unselectedLabelColor: Colors.grey,
-                  tabs: [
-                    Tab(icon: Icon(Icons.info), text: "Tab 1"),
-                    Tab(icon: Icon(Icons.lightbulb_outline), text: "Tab 2"),
-                  ],
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.light,
+        statusBarBrightness: Brightness.dark,
+      ),
+      child: Scaffold(
+        body: DefaultTabController(
+          length: 3,
+          child: CustomScrollView(
+            slivers: <Widget>[
+              SliverPersistentHeader(
+                pinned: true,
+                floating: false,
+                delegate: NetworkingPageHeader(
+                  minExtent:
+                      kToolbarHeight + MediaQuery.of(context).padding.top,
+                  maxExtent: 250.0,
                 ),
               ),
-              pinned: true,
-            ),
-            SliverFixedExtentList(
-              itemExtent: 50.0,
-              delegate: SliverChildBuilderDelegate((context, index) {
-                String _title = "List item $index";
-                return ListTile(
-                  title: Text(_title),
-                );
-              }),
-            ),
-          ],
+              SliverPersistentHeader(
+                delegate: SliverAppBarDelegate(
+                  TabBar(
+                    labelColor: Colors.black87,
+                    unselectedLabelColor: Colors.grey,
+                    tabs: [
+                      Tab(icon: Icon(Icons.info), text: "Tab 1"),
+                      Tab(icon: Icon(Icons.lightbulb_outline), text: "Tab 2"),
+                      Tab(icon: Icon(Icons.lightbulb_outline), text: "Tab 3"),
+                    ],
+                  ),
+                ),
+                pinned: true,
+              ),
+              TabBarView(
+                children: [
+                  Container(
+                    child: ListView.builder(itemBuilder: (_, index) {
+                      String _title = "List item $index";
+                      return ListTile(
+                        title: Text(_title),
+                      );
+                    }),
+                  ),
+                  Container(
+                    child: ListView.builder(itemBuilder: (_, index) {
+                      String _title = "List item $index";
+                      return ListTile(
+                        title: Text(_title),
+                      );
+                    }),
+                  ),
+                  Container(
+                    child: ListView.builder(itemBuilder: (_, index) {
+                      String _title = "List item $index";
+                      return ListTile(
+                        title: Text(_title),
+                      );
+                    }),
+                  ),
+                  // SliverFixedExtentList(
+                  //   itemExtent: 50.0,
+                  //   delegate: SliverChildBuilderDelegate((context, index) {
+                  //     String _title = "List item $index";
+                  //     return ListTile(
+                  //       title: Text(_title),
+                  //     );
+                  //   }),
+                  // ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
